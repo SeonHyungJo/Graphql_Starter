5e8bfa98c376ffa835af7e37d677fbe1d06b0d90
SeonHyungJo <seonhyungjo@gmail.com>
2018-08-13 13:18:47 +0900

    express graphql resolver연결 성공

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,14 @@
 import { people, getById, getAll } from "./db"
 
 const resolver = {
-  Query: {
     people: () => {
       let result = getAll()
       return result
     },
-    person: (_, { id }) => getById(id)
-  }
+    person: (_, { id }) => getById(id),
+    hello: () => {
+      return 'Hello world!';
+    }
 }
 
 export default resolver
--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-import { GraphQLServer } from "graphql-yoga"
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
 import resolvers from "./graphql/resolvers"
+//const schema = require('./graphql/schema')
+var { buildSchema } = require('graphql');
 
-const server = new GraphQLServer({
-  typeDefs: "./graphql/schema.graphql",
-  resolvers
-})
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
-server.start(() => console.log("Server is running on localhost:4000"))
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+var app = express();
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true,
+}));
+
+// import { GraphQLServer } from "graphql-yoga"
+//import resolvers from "./graphql/resolvers"
+
+// const server = new GraphQLServer({
+//   typeDefs: "./graphql/schema.graphql",
+//   resolvers
+// })
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+//server.start(() => console.log("Server is running on localhost:4000"))
 //--------------------------------------
 // connect To DB
 // import models from "./models"
